b2c182d4d9a5ce4bbafef576e46c860c6610bca4
Muhammed khoyer <muhammedkhoyer@Muhammeds-MacBook-Pro.local>
2019-08-06 22:18:51 -0400

    initial commit

---
+++ src/base/Variables.java
@@ -0,0 +1,15 @@
+package base;
+
+public class Variables {
+     public static void main(String[] args) {
+	 System.out.println("Hello World");
+     
+	 System.out.println("Peole NTech");
+     
+	 System.out.println("Peole NTech");
+	 
+	 System.out.println("Khoyer");
+	 System.out.println("khoyer1199@gmail.com");
+	 System.out.println("3754_NY");
+     }	 
+}
